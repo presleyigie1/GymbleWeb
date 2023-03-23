0f94352e3030d83b66acef82ad318f1e257a96fc
kevinosifo <kevinosifo@gmail.com>
2023-03-23 01:00:21 +0000

    Swipe page 90% complete -kevin

--- card.js
+++ card.js
@@ -1,8 +1,14 @@
 class Card{
     constructor({
         imageUrl,
+        onDismiss,
+        onLike,
+        onDislike
     }){
         this.imageUrl = imageUrl;
+        this.onDismiss = onDismiss;
+        this.onLike = onLike;
+        this.onDislike = onDislike;
         this.#init();
     }
 
@@ -28,10 +34,18 @@ class Card{
         this.element.addEventListener('mousedown',e=>{
            const {clientX, clientY}=e;
            this.#startPoint = {x:clientX,y:clientY};
+           //no transition when moving
+           this.element.style.transition='';
            document.addEventListener('mousemove',this.#handleMouseMove);
         });
-
-
+       
+        // mouseup
+        document.addEventListener('mouseup',this.#handleMouseUp);
+        
+        // prevent drag
+        this.element.addEventListener('dragstart',e=>{
+            e.preventDefault();
+        });
     }
 
     #handleMouseMove=(e) => {
@@ -39,9 +53,56 @@ class Card{
         const {clientX, clientY} = e;
         this.#offsetX = clientX - this.#startPoint.x;
         this.#offsetY = clientY - this.#startPoint.y;
+
+        const rotate = this.#offsetX * 0.1;
+
         this.element.style.transform = `translate(${this.
-        #offsetX}px, ${this.#offsetY}px)`;
+        #offsetX}px, ${this.#offsetY}px) rotate(${rotate}deg)`;
+
+        //dismiss card when moving too far away
+        if(Math.abs(this.#offsetX) > this.element.clientWidth * 0.7){
+            const direction = this.#offsetX > 0 ? 1 : -1;
+            this.#dismiss(direction);
+        }
        
 
+    }
+    #handleMouseUp= (e) => {
+        this.#startPoint = null;
+        document.removeEventListener('mousemove',this.#handleMouseMove);
+        // transition when move back
+        this.element.style.transition='transform 0.5s';
+        this.element.style.transform = '';
+
+
+    }
+
+    #dismiss = (direction)=>{
+        this.#startPoint = null;
+        document.removeEventListener('mouseup',this.#handleMouseUp);
+        document.removeEventListener('mousemove',this.#handleMouseMove);
+
+        this.element.style.transition = 'transform 1s';
+        this.element.style.transform = `translate(${direction * window.innerWidth}px,
+             ${this.#offsetY}px) rotate(${90 * direction}deg)`;
+             this.element.classList.add('dismissing');
+             setTimeout(()=>{
+               this.element.remove();
+             }, 1000);
+             
+             if(typeof this.onDismiss === 'function'){
+                this.onDismiss();
+             }
+             if(typeof this.onLike === 'function' && 
+             direction ===1){
+                this.onLike();
+                console.log('like')
+             }
+             if(typeof this.onDislike === 'function' &&
+             direction === -1){
+                this.onDislike();
+                console.log('dislike')
+             }
+                   
     }
 }
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// DOm
+// DOM
 const swiper = document.querySelector('#swiper');
+const like = document.querySelector('#like');
+const dislike = document.querySelector('#dislike');
 
 
 // constants
@@ -19,11 +21,29 @@ let cardCount = 0;
 // functions
 function appendNewCard(){
     const card = new Card({
-        imageUrl: urls[cardCount % 5]
+        imageUrl: urls[cardCount % 5],
+        onDismiss: appendNewCard,
+        onLike:() => {
+            like.style.animationPlayState = 'running';
+            // always trigger animation when toggling class
+            like.classList.toggle('trigger')
+
+        },
+        onDislike:()=>{
+            dislike.style.animationPlayState = 'running';
+            // always trigger animation when toggling class
+            dislike.classList.toggle('trigger')
+
+        }
     });
-    card.element.style.setProperty('--i',cardCount%5);
+   // card.element.style.setProperty('--i',cardCount%5);
     swiper.append(card.element);
     cardCount++;
+
+    const cards = swiper.querySelectorAll('.card:not(.dismissing');
+    cards.forEach((card,index)=>{
+        card.style.setProperty('--i', index);
+  });
 }
 
 //first 5 cards
