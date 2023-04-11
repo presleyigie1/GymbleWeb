9b4204d40b16fa4510703e85f13bbe8b91fd9853
Presley Igie <93250486+presleyigie1@users.noreply.github.com>
2023-04-11 01:26:25 +0100

    removed code

--- public/js/user.js
+++ public/js/user.js
@@ -24,34 +24,3 @@ logOut.onclick = () =>{
     sessionStorage.clear()
     location.reload()
 }
-
-
-class userProfile {
-    constructor(name, posts, matches, followers) {
-        this.name = name;
-        this.posts = posts;
-        this.matches = matches;
-        this.followers = followers;
-    }
-}
-
-const currentProfile = new userProfile("Emma", 6, 10, 23);//change the name to pull from the database
-
-//after the window loads, the function will run 
-//the function would not work without this, as the js was called before the HTML was loaded 
-window.onload = function () {
-    document.getElementById("userName").innerHTML = 
-    currentProfile.name;
-
-    document.getElementById("posts").innerHTML = 
-    currentProfile.posts;
-
-    document.getElementById("matches").innerHTML = 
-    currentProfile.matches;
-
-    document.getElementById("followers").innerHTML = 
-    currentProfile.followers;
-};
-
-
-
