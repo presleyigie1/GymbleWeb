60a0348a85c84823ca17b20a22dd4b0c47abc8e6
Presley Igie <93250486+presleyigie1@users.noreply.github.com>
2023-04-11 01:09:20 +0100

    created db and server, finished login and sign up

--- public/js/form.js
+++ public/js/form.js
@@ -1,5 +1,5 @@
 //shows and hides login/sign up forms
-function hideLog(){
+/*function hideLog(){
     var hide = document.getElementById("signin");
     hide.style.display = 'none';
 }
@@ -17,4 +17,98 @@ function hideSign(){
 function showSign(){
     var show = document.getElementById("signup");
     show.style.display = 'block';
+}*/
+
+/* animation for loading when switching forms
+const form = [...document.querySelector('signin').children];
+
+form.forEach((item, i ) => {
+    setTimeout(()=> {
+        item.style.opacity = 1;
+    }, i*100)
+})*/
+
+//form validation
+
+
+
+const fName = document.querySelector(".fName") || null
+const lName = document.querySelector(".lName") 
+const email = document.querySelector(".email") 
+const age = document.querySelector(".age")
+const password = document.querySelector(".password")
+const submitBtn = document.querySelector(".submit-btn")
+
+
+
+if(fName == null){ //it means the login page is open
+    submitBtn.addEventListener("click", () =>{
+        //using fetch to submit the form
+        fetch('/login-user',{
+            method: "post",
+            headers: new Headers({"Content-Type": "application/json"}),
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value
+            })
+        })
+        .then(res => res.json())
+        .then(data =>{
+            //displays the new registered users data in the console 
+            console.log(data);
+             validateData(data);
+        })
+    })
+
+}
+else{// means the register page is open
+    submitBtn.addEventListener('click', () =>{
+        //using fetch to submit the form
+        fetch('/register-user',{
+            method: "post",
+            //header is created to json
+            headers: new Headers({"Content-Type": "application/json"}),
+            //passing in our values on the register page to json
+            body: JSON.stringify({
+                fName: fName.value,
+                lName: lName.value,
+                age: age.value,
+                email: email.value,
+                password: password.value
+            })
+        })
+        //creatng response to json
+        .then(res => res.json())
+        //sending the data to our server.js file
+        .then(data =>{
+            //displays the new registered users data in the console 
+            console.log(data);
+            validateData(data);
+        })
+    })
+    
+}
+
+
+const validateData = (data) =>{
+    if(!data.fname){
+        alertBox(data)
+    }
+    else{
+        sessionStorage.fname = data.fname
+        sessionStorage.age = data.age
+        sessionStorage.name = data.email
+        location.href = "/swipe";
+    }
+}
+
+const alertBox = (data) =>{
+    const alertContainer = document.querySelector('.alert-box')
+    const alertMsg = document.querySelector(".alert-msg")
+    alertMsg.innerHTML = data 
+
+    alertContainer.style.top = "5%"
+    setTimeout(() =>{
+        alertContainer.style.top = ''
+    },5000)
 }
---
+++ public/js/user.js
@@ -0,0 +1,25 @@
+//adds the name of the user at top right of the screen next to the log out button
+const uName = document.querySelector(".uName")
+
+window.onload = () =>{
+    if(sessionStorage.name){
+        location.href = "/swipe"
+    }
+}
+
+window.onload = () => {
+    if(!sessionStorage.fname){
+        location.href = "/login"
+    }
+    else{
+        uName.innerHTML = `Hi ${sessionStorage.fname}` 
+    }
+}
+
+const logOut = document.querySelector(".logout-btn")
+
+//click on the log out button it will reload the page and clear the storage in the current session therefore logging
+logOut.onclick = () =>{
+    sessionStorage.clear()
+    location.reload()
+}
