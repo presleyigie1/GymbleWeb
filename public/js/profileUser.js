8a6e18f489197f707ce608d4e8328af642445508
emmakelly3001 <x21385283@student.ncirl.ie>
2023-04-12 23:40:56 +0100

    fixed bugs EK 12/04

--- public/js/profileUser.js
+++ public/js/profileUser.js
@@ -1,6 +1,5 @@
 /*class userProfile {
-    constructor(name, posts, matches, followers) {
-        this.name = name;
+    constructor(posts, matches, followers) {
         this.posts = posts;
         this.matches = matches;
         this.followers = followers;
@@ -8,7 +7,7 @@
 }*/
 
 
-//const currentProfile = new userProfile("Emma", 6, 10, 23);//change the name to pull from the database
+//const currentProfile = new userProfile(6, 10, 23);//change the name to pull from the database
 //after the window loads, the function will run 
 //the function would not work without this, as the js was called before the HTML was loaded
 
