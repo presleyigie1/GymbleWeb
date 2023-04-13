5c919f27113e053daa48c5012176edc7580f327a
emmakelly3001 <x21385283@student.ncirl.ie>
2023-04-13 12:36:10 +0100

    Emma Final 13/04

--- public/js/profileUser.js
+++ public/js/profileUser.js
@@ -1,34 +1,46 @@
-/*class userProfile {
+class userProfile {
     constructor(posts, matches, followers) {
         this.posts = posts;
         this.matches = matches;
         this.followers = followers;
     }
-}*/
+};
+
+const previewProfile = new userProfile(3, 15, 25);
+
+window.onload = function preview() {
+    document.getElementById("posts").innerHTML = 
+    currentProfile.posts;
+
+    document.getElementById("matches").innerHTML = 
+    currentProfile.matches;
+
+    document.getElementById("followers").innerHTML = 
+    currentProfile.followers;
+    
+};
 
 
-//const currentProfile = new userProfile(6, 10, 23);//change the name to pull from the database
+
+
+//const currentProfile = new userProfile("Flyefit", "Swords");//change the name to pull from the database
 //after the window loads, the function will run 
 //the function would not work without this, as the js was called before the HTML was loaded
 
 //emma connecting the profile page to the profile page 
 
 /*class previewProfile {
-    constructor(age, gym, gymLocation) {
-        this.age = age
+    constructor(gym, gymLocation) {
         this.gym = gym;
         this.gymLocation = gymLocation;
     }
 }
 
-const previewProfile = new userProfile(20, "GymPlus", "Swords");
+const previewProfile = new userProfile("GymPlus", "Swords");
 
 
 /*emma's function for the profile page*/
 /*window.onload = function preview() {
-    document.getElementById("age").innerHTML = 
-    currentProfile.age;
-
     document.getElementById("gym").innerHTML = 
     currentProfile.gym;
 
@@ -38,5 +50,4 @@ const previewProfile = new userProfile(20, "GymPlus", "Swords");
 };
 
 //need to get this to show in the preview profile page, and finish this and the settings page
-
 */
